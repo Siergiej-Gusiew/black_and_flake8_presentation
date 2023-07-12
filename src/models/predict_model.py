d9492fecac3e0d17d91421a64c89ac68d409234a
Sergiusz Gusiew <45262557+Siergiej-Gusiew@users.noreply.github.com>
2023-07-12 08:38:51 +0000

    codespace pre-commit test 2

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,9 +1,24 @@
-# lsdjfo;jspidf;asdfoasidfpaisdfoia;sjdfpi;asdpifuapisdf;asdf;asdfa;sdfoiaefpioaudsf
+# lsdjfo;jspidf;asdfoasidfpaisdfoia;sjdfpi;asdpkjsdhfhlkdjfgifuapisdf;asdf;asdfa;sdfoiaefpioaudsf
+# import pandas as pd
 
 
 def some_funk():
+    # arg = 2
     return 2
 
 
-def hey_there():
+def hey_there():  # klsdhf
     return "Hey there"
+
+
+def some_func():
+    kjsdhfkjhsoiudfuhsiudhfsdoihfiushdfiuphsweiuhsf = 2
+    kjhsdkjfhksdjhfiusdhuifhiusdhfiuhsidufhiushdf_2 = 3
+    if (
+        kjsdhfkjhsoiudfuhsiudhfsdoihfiushdfiuphsweiuhsf
+        + kjhsdkjfhksdjhfiusdhuifhiusdhfiuhsidufhiushdf_2
+        > 5
+    ):
+        return True
+    else:
+        return False
